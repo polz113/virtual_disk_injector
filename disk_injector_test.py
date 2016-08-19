4ef2a3567c894fd2b587b8e5462e7c2f181ff220
polz <polz@nell.stigma.predoslje.com>
2016-08-19 10:10:17 +0200

    Added the test script which runs on a VM

--- disk_injector_test.py
+++ disk_injector_test.py
@@ -8,12 +8,15 @@ from virtualdiskinjector import *
 import random
 from time import sleep
 
+md5_sum_fname = 'virtual_host_test_script/sdc_md5sum.txt'
+
 def __main__():
     N_WRITES = 256
     SEED = 31337
     DISK_SIZE = 3*(2**30)
     SECTOR_SIZE = 512
-    data = "C" * 32
+    static_data = "C" * 32
+    extending_data = "D" * 2**18
     """The woods are lovely, dark and deep
 but I have promises to keep
 and miles to go before I sleep.
@@ -26,44 +29,67 @@ and miles to go before I sleep.
     """
     hiding_spaces = dict()
     tester_disk_img = "../data/test_images/tester.vmdk"
-    for ending, img_type in [('qcow2', 'qcow2'),
-                             ('vdi', 'vdi'),
-                             ('vmdk', 'vmdk'),
-                             ('vhd', 'vpc')]:
-    # for ending in ['vhd']:
+    #for ending, img_type in [('qcow2', 'qcow2'),
+    #                         ('vdi', 'vdi'),
+    #                         ('vmdk', 'vmdk'),
+    #                         ('vhd', 'vpc')]:
+    for ending, img_type in [('qcow2', 'qcow2')]:
         hiding_spaces_list = []
         r = random.Random(SEED)
         img_fname = '../data/test_images/test1.'+ending
         print ending
         # create 3GiB image
         call(["qemu-img", "create", "-f", img_type, img_fname, str(DISK_SIZE)])
-        # write 256 As
-        call(["qemu-system-x86_64", "--enable-kvm", "-hda", "tester.vmdk", "-hdb", img_fname])
+        # run virtual_host_test_script/test_injection.py 
+        # in qemu, write 256 ? Bs ?
+        call(["qemu-system-x86_64", "--enable-kvm",
+                "-hda", tester_disk_img,
+                "-hdb", "fat:rw:virtual_host_test_script",
+                "-hdc", img_fname])
         # identify spaces
         c = create_hider(img_fname)
         fixed_spaces = c.fixed_hiding_spaces()
         for space in fixed_spaces:
             hiding_spaces_list.append(space)
-        print "Required space:", len(data)
+        print "Required space:", len(static_data)
         print "Fixed:", fixed_spaces
         extending_spaces = c.extending_hiding_spaces()
         for space in extending_spaces:
             hiding_spaces_list.append(space)
         hiding_spaces[img_type] = hiding_spaces_list
         print "Extending:", extending_spaces 
-        c.hide_fixed(fixed_spaces[0][0], data)
-        c.hide_extending(extending_spaces[0][0], 'D' * 1000000)
-        print "Data hidden"
+        call(['cp', img_fname, img_fname + '.orig'])
+        c.hide_fixed(fixed_spaces[0][0], static_data)
+        c.hide_extending(extending_spaces[0][0], extending_data)
+        with open(img_fname, 'r') as f:
+            f.seek(fixed_spaces[0][0])
+            d1 = f.read(len(static_data))
+            assert d1 == static_data
+            f.seek(extending_spaces[0][0])
+            d2 = f.read(len(extending_data))
+            print "extending pre-check"
+            # print d2
+            assert d2 == extending_data
         # write 256 Bs
-        call(["qemu-system-x86_64", "--enable-kvm", "-hda", "tester.vmdk", "-hdb", img_fname])
+        call(['cp', img_fname, img_fname + '.bak'])
+        # run virtual_host_test_script/test_injection.py 
+        # for a second time, write 256 ? Bs ?
+        call(["qemu-system-x86_64", "--enable-kvm",
+                "-hda", tester_disk_img,
+                "-hdb", "fat:rw:virtual_host_test_script",
+                "-hdc", img_fname])
+
         # check the hidden data
         with open(img_fname, 'r') as f:
             f.seek(fixed_spaces[0][0])
-            d1 = f.read(len(data))
-            assert d1 == data
-            f.seek(fixed_spaces[0][0])
-            d2 = f.read(1000000)
-            assert d2 == 'D' * 1000000
+            d1 = f.read(len(static_data))
+            assert d1 == static_data
+            f.seek(extending_spaces[0][0])
+            d2 = f.read(len(extending_data))
+            print "extending check"
+            print d2
+            assert d2 == extending_data
+        os.unlink(md5_sum_fname)
         # c.hide_extending(fixed_spaces[0][0], data)
         # print unicode(c)
     print hiding_spaces
---
+++ virtual_host_test_script/test_injection.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+
+import random
+from subprocess import call, check_output
+import hashlib
+import os
+import datetime
+
+md5_sum_fname = "/mnt/test_script/sdc_md5sum.txt"
+log_fname_format = "/mnt/test_script/log_{:0>3}.txt"
+disc_fname = "/dev/sdc"
+BLOCK_SIZE = 2**20 # 2**20
+N_WRITES = 2 # *(2**4)
+hash_fn = hashlib.md5
+
+if hasattr(os, 'sync'):
+    sync = os.sync
+else:
+    import ctypes
+    libc = ctypes.CDLL("libc.so.6")
+    def sync():
+        libc.sync()
+
+def find_free_log_fname():
+    log_n = 1
+    log_fname = log_fname_format.format(log_n)
+    while os.path.isfile(log_fname):
+        log_n += 1
+        log_fname = log_fname_format.format(log_n)
+    return log_fname
+
+def log(s):
+    with open(log_fname, "a") as f:
+        print("log:" + s)
+        f.write(s + "\n")
+    sync()
+
+def disc_hash(fname):
+    h = hash_fn()
+    with open(fname, 'r') as f:
+        s = f.read(BLOCK_SIZE)
+        while len(s):
+            h.update(s)
+            s = f.read(BLOCK_SIZE)
+    return h.hexdigest()
+
+def write_randomly(fname, n, s, r):    
+    disc_size = int(check_output(["blockdev", "--getsize64", fname]))
+    log("size:" + str(disc_size))
+    with open(disc_fname, 'w') as f:
+        for i in xrange(n):
+            fpos = r.randint(0, disc_size-1)
+            f.seek(fpos)
+            f.write(s)
+            log("wrote {} at {}".format(s, fpos))
+
+
+log_fname = find_free_log_fname()
+log("Run:" + datetime.datetime.now().isoformat())
+
+try:
+    with open(md5_sum_fname, "r") as f:
+        hash_sum_str = f.read()
+except Exception, e:
+    log("Hash sum file missing")
+    hash_sum_str = None
+    
+if hash_sum_str is None:
+    r = random.Random(0xbadc0de)
+    write_randomly(disc_fname, N_WRITES, 'A', r)
+
+h = disc_hash(disc_fname)
+
+if hash_sum_str is None:
+    with open(md5_sum_fname, "w") as f:
+        f.write(h)
+    call(["poweroff"])
+else:
+    print "heeeere"
+    s1 = str(h) + " ?= " + hash_sum_str
+    print s1
+    log(s1)
+    r = random.Random(31337)
+    write_randomly(disc_fname, N_WRITES, 'B', r)
+    # os.unlink(md5_sum_fname)
+    call(["poweroff"])
